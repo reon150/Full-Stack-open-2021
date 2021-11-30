1940b949f851e796327231cc58c6329eb0363ad0
Ronald Ogando <ronaldogando150@gmail.com>
2021-11-30 14:18:18 -0400

    2.1: Course information step6

---
+++ part02/course-information/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const Header = ({ course }) =>  <h1>{course.name}</h1>
+
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>    
+)
+
+const Content = ({ course }) => (
+  <div>
+    <Part part={course.parts[0]} />
+    <Part part={course.parts[1]} />
+    <Part part={course.parts[2]} />
+  </div>
+)
+
+const Course = course => (
+  <div>
+    <Header course={course} />
+    <Content course={course} />
+    <Total course={course} />
+  </div>
+)
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+
+export default App;
