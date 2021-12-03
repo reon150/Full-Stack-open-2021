e0edeaf7b2cab4e9e7db4df6b784dc3ad697b977
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-03 18:21:42 -0400

    The Phonebook Step4
    Implement a search field that can be used to filter the list of people by name

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -2,41 +2,55 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [personsToShow, setPersonsToShow] = useState([...persons])
+  const [formState, setFormState] = useState({ name: '', number: '' })
+  const [search, setSearch] = useState('')
 
-  const handlNameChange = (event) => setNewName(event.target.value)
-  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFormChange = (event) => setFormState({
+    ...formState,
+    [event.target.name]: event.target.value
+  })
 
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+    if (persons.some(person => person.name === formState.name)) {
+      alert(`${formState.name} is already added to phonebook`)
     } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }))
-      setNewName('')
-      setNewNumber('')
+      const newPersons = persons.concat({ name: formState.name, number: formState.number, id: persons[persons.length - 1].id + 1 })
+      setPersons(newPersons)
+      setFormState({ name: '', number: '' })
+      setSearch('')
+      setPersonsToShow(newPersons)
     }
+  }
+
+  const filterPhoneBook = (event) => {
+    setSearch(event.target.value)
+    setPersonsToShow(persons.filter(person => person.name.toLowerCase().includes(event.target.value)))
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input value={search} onChange={filterPhoneBook} />
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handlNameChange} />
+          name: <input name='name' value={formState.name} onChange={handleFormChange} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
+          number: <input name='number' value={formState.number} onChange={handleFormChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {personsToShow.map(person => <div key={person.id}>{person.name} {person.number}</div>)}
     </div>
   )
 }
