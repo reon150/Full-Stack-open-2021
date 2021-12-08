f5b5b6fce74731deef7c915e975234caa96ad8f1
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-07 22:26:22 -0400

    2.11: The Phonebook Step6
    Store the initial state of the application in the file db.json. Start json-server on port 3001 and make sure that the server returns the list of people by going to the address http://localhost:3001/persons in the browser. Modify the application such that the initial state of the data is fetched from the server using the axios-library.

--- part02/phonebook/src/components/App.js
+++ part02/phonebook/src/components/App.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
+import axios from 'axios'
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [personsToShow, setPersonsToShow] = useState([...persons])
   const [formState, setFormState] = useState({ name: '', number: '' })
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)  
+        setPersonsToShow(response.data)
+      })
+  }, [])
 
   const handleFormChange = (event) => setFormState({
     ...formState,
