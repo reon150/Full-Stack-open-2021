dacddaac3a16c21e85510766420728293b851cd7
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-11 12:36:45 -0400

    3.9 phonebook backend step9

--- part02/phonebook/src/services/persons.js
+++ part02/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = process.env.REACT_APP_PHONEBOOK_API || 'http://localhost:3001/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
