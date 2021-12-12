196267d2b3d14188343dc9054bb73c4d0da2924c
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-11 23:04:59 -0400

    3.12: Command-line database

---
+++ part03/phonebook-api/mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/phonebook'
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 2) {
+  Person.find({}).then(persons => {
+    console.log('phonebook:')
+    persons.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const person = new Person({
+    name: process.argv[2],
+    number: process.argv[3]
+  })
+  
+  person.save().then(person => {
+    console.log(`added ${person.name} ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
+
