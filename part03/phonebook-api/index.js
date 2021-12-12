54e0ff591db689d92fd716cdf853a53d6f5543e2
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-12 00:44:25 -0400

    3.18*: Phonebook database step6

--- part03/phonebook-api/index.js
+++ part03/phonebook-api/index.js
@@ -11,42 +11,20 @@ app.use(express.json())
 morgan.token('body', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then(persons => res.json(persons))
+app.get('/api/persons', (req, res, next) => {
+  Person.find({}).then(persons => res.json(persons)).catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id == id)
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(400).end()
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(400).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
@@ -54,10 +32,6 @@ app.post('/api/persons', (req, res, next) => {
   if (!body.name || !body.number) {
     res.status(400).json({
       error: 'content missing'
-    })
-  } else if (persons.find(person => person.name == body.name)) {
-    res.status(409).json({
-      error: 'name must be unique'
     })
   } else {
     const person = new Person({
@@ -91,10 +65,12 @@ app.delete('/api/persons/:id', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-  res.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date}</p>
-  `)
+  Person.count({}, (err, count) => {
+    res.send(`
+      <p>Phonebook has info for ${count} people</p>
+      <p>${new Date}</p>
+    `)
+  })
 })
 
 app.use((error, request, response, next) => {
