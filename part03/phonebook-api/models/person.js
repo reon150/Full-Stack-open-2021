082a83f724d3eade01b1b3ee5e278ed87353fec4
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-12 14:45:03 -0400

    3.19: Phonebook database, step7

--- part03/phonebook-api/models/person.js
+++ part03/phonebook-api/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/phonebook'
 
@@ -13,9 +14,15 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: { 
+    type: String, 
+    required: true,
+    unique: true
+  },
   number: String
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
