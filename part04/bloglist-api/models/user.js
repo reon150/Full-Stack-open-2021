a253b43f8ce7ff607d684be3b008b3237a90df76
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-21 18:17:53 -0400

    4.15: bloglist expansion, step3

---
+++ part04/bloglist-api/models/user.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+  },
+  name: String,
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog',
+    },
+  ],
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash;
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- part04/bloglist-api/utils/config.js
+++ part04/bloglist-api/utils/config.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3003;
 const MONGODB_URI = process.env.NODE_ENV === 'test'
-  ? process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/phonebook_tests'
-  : process.env.MONGODB_URI || 'mongodb://localhost:27017/phonebook';
+  ? process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/blogs_tests'
+  : process.env.MONGODB_URI || 'mongodb://localhost:27017/blogs';
 
 module.exports = {
   MONGODB_URI,
