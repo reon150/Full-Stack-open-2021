3a527a7be9a49f407b1f878a1217d59bc17bd310
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-20 23:29:17 -0400

    4.14 Blog list expansions, step2

--- part04/bloglist-api/controllers/blogs.js
+++ part04/bloglist-api/controllers/blogs.js
@@ -6,21 +6,47 @@ blogsRouter.get('/', async (request, response) => {
   return response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response) => {
-  request.body.likes = request.body.likes === undefined ? 0 : request.body.likes;
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    request.body.likes = request.body.likes === undefined ? 0 : request.body.likes;
 
-  if (!request.body.title || !request.body.author || !request.body.url) {
-    return response.status(400).json({ error: 'title, author or url missing' });
+    const blog = new Blog(request.body);
+    const blogSaved = await blog.save();
+
+    return response.status(201).json(blogSaved);
+  } catch (error) {
+    return next(error);
   }
-
-  const blog = new Blog(request.body);
-  const blogSaved = await blog.save();
-  return response.status(201).json(blogSaved);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id);
   response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  try {
+    const {
+      title,
+      author,
+      url,
+      likes,
+    } = request.body;
+
+    request.body.likes = request.body.likes === undefined ? 0 : request.body.likes;
+
+    const blog = {
+      title,
+      author,
+      url,
+      likes,
+    };
+
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+    return response.json(updatedBlog);
+  } catch (error) {
+    return next(error);
+  }
 });
 
 module.exports = blogsRouter;
