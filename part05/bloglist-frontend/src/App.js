5a2abae615f8ed076ca58d2b77981db14f26b25c
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-21 23:41:25 -0400

    5.1: bloglist frontend, step1

---
+++ part05/bloglist-frontend/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    const user = await loginService.login({
+      username, password,
+    })
+
+    blogService.setToken(user.token)
+
+    setUser(user)
+    setUsername('')
+    setPassword('')
+  }
+
+  if (user === null) {
+    return (
+      <LoginForm
+        username={username}
+        password={password}
+        handleUsernameChange={({ target }) => setUsername(target.value)}
+        handlePasswordChange={({ target }) => setPassword(target.value)}
+        handleSubmit={handleLogin}
+      />
+    )
+  }
+
+  blogService.getAll()
+    .then(blogs => setBlogs(blogs))
+  
+  return (
+    <div>
+      <h2>blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
