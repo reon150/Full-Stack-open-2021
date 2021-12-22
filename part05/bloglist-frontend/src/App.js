035d3d10506f80bbc7383e4c6cc0748d50529a92
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-22 19:34:49 -0400

    5.2: bloglist frontend, step2

--- part05/bloglist-frontend/src/App.js
+++ part05/bloglist-frontend/src/App.js
@@ -10,6 +10,21 @@ const App = () => {
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+
+      blogService
+        .getAll()
+        .then(blogs => {
+          setBlogs(blogs)
+        })
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -17,11 +32,25 @@ const App = () => {
       username, password,
     })
 
+    window.localStorage.setItem(
+      'loggedNoteappUser', JSON.stringify(user)
+    )
+
     blogService.setToken(user.token)
+    blogService
+        .getAll()
+        .then(blogs => {
+          setBlogs(blogs)
+        })
 
     setUser(user)
     setUsername('')
     setPassword('')
+  }
+
+  const handleLogOut = () => {
+    window.localStorage.removeItem('loggedNoteappUser')
+    setUser(null)
   }
 
   if (user === null) {
@@ -35,14 +64,11 @@ const App = () => {
       />
     )
   }
-
-  blogService.getAll()
-    .then(blogs => setBlogs(blogs))
   
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={handleLogOut}>log out</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
