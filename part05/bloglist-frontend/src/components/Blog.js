bbd8916bf79a20d02147ceb71662b95833c5f2a0
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-22 21:47:49 -0400

    5.7 Blog list frontend, step7

--- part05/bloglist-frontend/src/components/Blog.js
+++ part05/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleShowDetails}>{showDetails ? 'hide': 'view'}</button>
+      </div>
+      {showDetails ? 
+        <div>
+          <div>
+            {blog.url}
+          </div>
+          <div>
+            likes {blog.likes}
+          </div>
+          <div>
+            {blog.user.name}
+          </div>
+        </div>  :
+        <></>
+      }
+    </div>
+  )
+}
 
 export default Blog
