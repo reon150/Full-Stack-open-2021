18fd73ebf2a57acc70b28a0d77a56e6c1937eb2c
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-23 23:00:53 -0400

    5.17: bloglist end to end testing, step1

--- part05/bloglist-frontend/src/components/LoginForm.js
+++ part05/bloglist-frontend/src/components/LoginForm.js
@@ -12,7 +12,7 @@ const LoginForm = ({
     <div>
       <h2>Login</h2>
 
-      <form onSubmit={handleSubmit}>
+      <form id='login-form' onSubmit={handleSubmit}>
         <div>
           username
           <input
