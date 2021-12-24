d93d1dcf8a6107b157ce4e4bdf1380504f09eaee
Ronald Ogando <ronaldogando150@gmail.com>
2021-12-23 23:09:05 -0400

    5.18: bloglist end to end testing, step2

--- part05/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part05/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,13 @@
+const user = {
+  name: 'Ronald Ogando',
+  username: 'reon150',
+  password: '123'
+}
+
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +16,28 @@ describe('Blog app', function() {
     cy.contains('Login')
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type(user.username)
+      cy.get('#password').type(user.password)
+      cy.get('#login-button').click()
+      cy.contains(`${user.name} logged in`)
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type(user.username)
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'Wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', `${user.name} logged in`)
+    })
   })
 })
