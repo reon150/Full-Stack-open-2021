c1801b708489e8f3a7b0f8b3cb9677a0b5782017
Ronald Ogando <ronaldogando150@gmail.com>
2022-01-01 12:19:30 -0400

    8.5: Books by genre

--- part08/library-backend/index.js
+++ part08/library-backend/index.js
@@ -97,7 +97,10 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(
+      author: String
+      genre: String
+    ): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -107,8 +110,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) return books
-      return books.filter(book => book.author === args.author) 
+      let filteredBooks = books;
+      if (args.author)
+        filteredBooks = filteredBooks.filter(b => b.author === args.author)
+      
+      if (args.genre)
+        filteredBooks = filteredBooks.filter(b => b.genres.includes(args.genre))
+      
+      return filteredBooks
     },
     allAuthors: () => authors,
   },
