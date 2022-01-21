d97343a956228bf058117241a6963c8a47b428cb
Ronald Ogando <ronaldogando150@gmail.com>
2022-01-21 18:47:47 -0400

    8.19 Books by genre, part 1

--- part08/library-frontend/src/components/Books.js
+++ part08/library-frontend/src/components/Books.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS)
+  const [books, setBooks] = useState([])
+  const [filteredBooks, setFilteredBooks] = useState([])
+  const [genres, setGenres] = useState([])
+  const [selectedGenre, setSelectedGenre] = useState('All')
+
+  useEffect(() => {
+    if (!result.loading) {
+      const allBooks = result.data.allBooks
+      setBooks(allBooks)
+      let genres = ['All']
+      allBooks.forEach(book => {
+        book.genres.forEach((genre) => {
+          if (genres.indexOf(genre) === -1) {
+            genres.push(genre)
+          }
+        })
+      })
+      setGenres(genres)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [result.data])
+
+  useEffect(() => {
+    if (selectedGenre === 'All') {
+      setFilteredBooks(books)
+    } else {
+      setFilteredBooks(
+        books.filter((book) => book.genres.indexOf(selectedGenre) !== -1)
+      )
+    }
+  }, [books, selectedGenre])
 
   if (result.loading)  {
     return <div>loading...</div>
@@ -12,8 +43,6 @@ const Books = (props) => {
   if (!props.show) {
     return null
   }
-
-  const books = result.data.allBooks
 
   return (
     <div>
@@ -30,7 +59,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
+          {filteredBooks.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -39,6 +68,14 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+      <div>
+        {genres.length > 0 &&
+          genres.map((genre) => (
+            <button onClick={() => setSelectedGenre(genre)} key={genre}>
+              {genre}
+            </button>
+          ))}
+      </div>
     </div>
   )
 }
